8d89fd980728da2942134c9582318395f1d14e3a
Ontheroad123 <31181026+Ontheroad123@users.noreply.github.com>
2023-11-12 10:37:58 +0800

    Create pdf2png.py

---
+++ pdf2png.py
@@ -0,0 +1,15 @@
+#将pdf转为png
+from pdf2image import convert_from_path
+import os 
+import os.path as osp
+
+savename = ''
+pdfname = ''
+for fil in os.listdir(pdfname):
+  onepdf = osp.join(pdfname, fil)
+  savepdf = osp.join(savename, fil)
+  if not osp.exists(savepdf):
+    os.makedirs(savepdf)
+    pages = convert_from_path(onepdf, 300, savepdf, fmt='png', output_file='ok', thread_count=8)
+   
+
